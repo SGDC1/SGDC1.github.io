938c5e46b76dd70022aaf6770db7e7f7f5dc7161
SGDC1 <831194+1@apps.district196.org>
2024-06-26 21:46:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var element = document.documentElement;
 
+function optionsMenu() {
+  if(prompt("Options: \n 1. Fullscreen") == 1) {
+    if(element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
