339569563f943323b09dbde8d70a60fa553721fe
SGDC1 <831194+1@apps.district196.org>
2024-06-27 18:15:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var element = document.documentElement;
 
-function optionsMenu() {
-  option = prompt("Options: \n 1. Fullscreen");
-  if(option == 1) {
-    if(element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.webkitRequestFullscreen) {
+function openFullscreen() {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
     element.webkitRequestFullscreen();
   } else if (element.msRequestFullscreen) {
     element.msRequestFullscreen();
