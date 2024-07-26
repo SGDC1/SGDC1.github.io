0cbcb8918587158375c8b5d333fa8b3700009625
SGDC1 <831194+1@apps.district196.org>
2024-07-26 12:59:46 -0500

    Create main.js

---
+++ gd/main.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function setup() {
+  draw("img", 0, 0, "player.png"); //player
+  draw("img", 0, 250, "ground.png"); //ground
+}
+
+function draw(type, x, y, src) {
+  switch (type) {
+    case "img" {
+      let img = new Image();
+      img.src = src;
+      img.onload = () => ctx.drawImage(img, x, y);
+      break;
+    }
+    default:
+      console.log("Switch default;");
+  }
+}
